83a8220ef228953575e927616a8f6fbf9976e354
Ankit <ankitbhattarai.ab@protonmail.com>
2023-12-28 17:30:01 +0000

    Preprocessing for SDT from tsv to training ready json

--- scripts/preprocess_sdt/prepare_sgd_sdt_data.sh
+++ scripts/preprocess_sdt/prepare_sgd_sdt_data.sh
@@ -3,15 +3,16 @@
 # the project root
 FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
-echo "Raw datasets will be put in $DIR/data/raw/"
-
-# the temp directory used, within $DIR
-# omit the -p parameter to create a temporal directory in the default location
-WORK_DIR=`mktemp -d -p "$DIR"`
 
 # The directory where the raw SGD data will be placed
 raw_dir=$DIR/data/raw/SGD
 mkdir -p $raw_dir
+
+echo "Raw datasets will be put in $raw_dir"
+
+# the temp directory used, within $DIR
+# omit the -p parameter to create a temporal directory in the default location
+WORK_DIR=`mktemp -d -p "$DIR"`
 
 # check if tmp dir was created
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
@@ -33,19 +34,32 @@ wget --directory-prefix=$WORK_DIR https://github.com/google-research-datasets/ds
 unzip $WORK_DIR/master.zip -d $WORK_DIR
 mv $WORK_DIR/dstc8-schema-guided-dialogue-master/* $raw_dir
 
-python_script=$FILE_DIR/create_sgd_sdt.py
+create_script=$FILE_DIR/create_sgd_sdt.py
+preprocess_script=$FILE_DIR/preprocess_sdt_sgd.py
+
 interim_dir=$DIR/data/interim/SGD_SDT
+processed_dir=$DIR/data/processed/SGD_SDT
+
+preprocessing_config=$DIR/configs/data_processing_sdt_sgd.yaml
+
 mkdir -p $interim_dir
+mkdir -p $processed_dir
 
 for subdir in "train" "test" "dev"
 do
   for indices in "0" "1" "2" "3" "4"
   do
     echo $subdir-$indices
-    output_path=$interim_dir/v"$indices"_"$subdir".tsv
-    echo $output_path
-    python $python_script --input_dir=$raw_dir --output_path=$output_path --prompt_indices=$indices \
+    data_create_path=$interim_dir/v"$indices"_"$subdir".tsv
+    echo $data_create_path
+    python $create_script --input_dir=$raw_dir --output_path=$data_create_path --prompt_indices=$indices \
     --mcq_cat_vals --subdirs=$subdir
+    output_path=$processed_dir/v"$indices/$subdir"
+    python $preprocess_script --data_path=$data_create_path --raw_data_path=$raw_dir \
+    --prompt_indices=$indices --output_path=$output_path --config=$preprocessing_config
   done
 done
+
+# Delete Interim Folder after processing is done
+rm -rf $interim_dir
 
