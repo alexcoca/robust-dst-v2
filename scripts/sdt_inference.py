6ebf72ab42b5a816c67af6db1f71ce51ceb0fba8
alexcoca <alexcoca23@yahoo.co.uk>
2025-02-03 11:35:33 +0000

    Update: offline decoding scripts

---
+++ scripts/sdt_inference.py
@@ -0,0 +1,247 @@
+# Work by Ankit Bhattarai as part of Conversational AI Development project
+import json
+import logging
+import pathlib
+import re
+from collections import OrderedDict
+
+import click
+import numpy as np
+import torch
+from tqdm import tqdm
+from transformers import (
+    T5ForConditionalGeneration,
+    T5TokenizerFast,
+)
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+## Note that this file will only work with the delimiters in place
+example_slots_regex = r"\[slots\](.*)\[context\]"
+individual_slots_regex = r"(\w+)=([^;]+)"
+mwoz_individual_slots_regex = r"(\w+-\w+)=(\S.*?(?= \w+-|$))"
+
+
+def load_data(data_path, num_samples=-1, start_index=0):
+    with open(data_path, "r") as f:
+        data = json.load(f)
+    data = data["data"]
+    if num_samples == -1:
+        return data
+    else:
+        return data[start_index : start_index + num_samples]
+
+
+def load_model_tokenizer(model_name_or_path):
+    model = T5ForConditionalGeneration.from_pretrained(
+        model_name_or_path, cache_dir="cache"
+    )
+    tokenizer = T5TokenizerFast.from_pretrained(model_name_or_path, cache_dir="cache")
+    return model, tokenizer
+
+
+def tokenize_slots(slot_text, tokenizer, not_first_slot=True):
+    if not_first_slot:
+        slot_text = "=" + slot_text
+    tokenized = tokenizer(slot_text, add_special_tokens=False, return_tensors="pt")[
+        "input_ids"
+    ].flatten()
+    if not_first_slot:
+        tokenized = tokenized[1:]
+    return tokenized
+
+
+def tokenize_constraints(constraint_text, tokenizer):
+    constraint_text = "=" + constraint_text
+    tokenized = tokenizer(
+        constraint_text, add_special_tokens=False, return_tensors="pt"
+    )["input_ids"].flatten()
+    return tokenized[1:].item()
+
+
+def get_constraints(slots, categorical_slots_mapping, tokenizer):
+    logger.info("Tokenizing constraints")
+    all_constraints = []  # Constraints for each example stored as a list
+    tokenized_dict = {}  # A lookup table of slot / categorical values already tokenized
+    for turn_slots, mapping in tqdm(zip(slots, categorical_slots_mapping)):
+        constraint = OrderedDict()
+        mapping = eval(mapping) if mapping else {}
+        for i, slot in enumerate(turn_slots):
+            not_first = bool(i)
+            not_first = False  # Check this out!
+            slot_tokenized = tokenized_dict.setdefault(
+                (slot, not_first), tokenize_slots(slot, tokenizer, not_first)
+            )
+            if slot in mapping:
+                slot_constraints = []
+                options = list(mapping[slot].keys())
+                for option in options:
+                    option_tokenized = tokenized_dict.setdefault(
+                        option, tokenize_constraints(option, tokenizer)
+                    )
+                    slot_constraints.append(option_tokenized)
+            else:
+                slot_constraints = None
+            constraint[slot_tokenized] = slot_constraints
+        all_constraints.append(constraint)
+    logger.info("Tokenizing constraints done")
+    return all_constraints
+
+
+def get_sorted_data(data, tokenizer):
+    source = [item["source"] for item in data]
+    source_ids = tokenizer(source)["input_ids"]
+    len_source = np.array([len(src) for src in source_ids])
+    sorting_index = np.argsort(len_source)[::-1]  # Sorting by reverse order
+    source = [source[i] for i in sorting_index]
+    categorical_slots_mapping = [
+        data[i]["categorical_slots_mapping"] for i in sorting_index
+    ]
+    reference_slot_values = [re.findall(example_slots_regex, src)[0] for src in source]
+
+    slots_regex = individual_slots_regex
+    slots = [
+        [slot for slot, _ in re.findall(slots_regex, ref.replace("[SEP]", ";"))]
+        for ref in reference_slot_values
+    ]
+    source_ids = [source_ids[i] for i in sorting_index]
+    return source, source_ids, categorical_slots_mapping, slots, sorting_index
+
+
+def dynamic_batches(
+    source,
+    tokenizer,
+    start_batch_size=128,
+    max_target_length=140,
+    safety_factor=0.95,
+    fixed_batch_size=False,
+):
+    batches = []
+    len_source = len(tokenizer(source[0])["input_ids"])
+    start_max_source_size = np.max(len_source)
+    current_max_length = start_max_source_size
+    batch_size = start_batch_size
+    i = 0
+    while True:
+        if len(source) == 0:
+            break
+        if i != 0:
+            n = start_batch_size * (start_max_source_size + max_target_length)
+            start_item = source[0]
+            current_max_length = len(tokenizer(start_item)["input_ids"])
+            n /= current_max_length + max_target_length
+            batch_size = (
+                np.maximum(int(n * safety_factor), 1)
+                if not fixed_batch_size
+                else start_batch_size
+            )
+        log_text = f"Batch: {i}, Batch size: {batch_size}, Current max length: {current_max_length}"
+        logger.info(log_text)
+        current_slice = source[:batch_size]
+        tokenized = tokenizer(current_slice, padding=True, return_tensors="pt")
+        batches.append(tokenized)
+        source = source[batch_size:]
+        i += 1
+    return batches
+
+
+
+@torch.no_grad()
+def decode(
+    model, tokenizer, batches, device, max_target_length=140
+):
+    outputs = []
+    model = model.to(device)
+    model.eval()
+    for batch in tqdm(batches):
+        input_ids = batch["input_ids"].to(device)
+        attention_mask = batch["attention_mask"].to(device)
+        model_output = model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            max_length=max_target_length,
+        )
+        decoded = tokenizer.batch_decode(model_output, skip_special_tokens=False)
+        outputs.extend(decoded)
+    return outputs
+
+
+def reorder_outputs(outputs, sorting_index):
+    reverse_mapping = {v: i for i, v in enumerate(sorting_index)}
+    sorted_outputs = [outputs[reverse_mapping[i]] for i in range(len(sorting_index))]
+    sorted_outputs = [
+        item.replace("<pad>", "").replace("</s>", "").strip() for item in sorted_outputs
+    ]
+    return sorted_outputs
+
+
+@click.command()
+@click.option("--test_file", type=click.Path(exists=True), required=True)
+@click.option("--model_name_or_path", type=str, required=True)
+@click.option("--output_dir", type=click.Path(exists=True), required=True)
+@click.option("--start_batch_size", type=int, default=128, required=False)
+@click.option("--max_target_length", type=int, default=140, required=False)
+@click.option("--safety_factor", type=float, default=0.97, required=False)
+@click.option(
+    "--num_samples",
+    type=int,
+    default=-1,
+    required=False,
+    help="Number of samples to run the inference on. If left as -1, it will run on the entire dataset",
+)
+@click.option(
+    "--start_index",
+    type=int,
+    default=0,
+    required=False,
+    help="Start index for the dataset - good for debugging",
+)
+@click.option(
+    "--fixed_batch_size",
+    type=bool,
+    default=False,
+    required=False,
+    help="If true, the batch size will be fixed at the start_batch_size",
+)
+def main(
+    test_file: pathlib.Path,
+    model_name_or_path: str,
+    output_dir: pathlib.Path,
+    start_batch_size: int,
+    max_target_length: int,
+    safety_factor: float,
+    num_samples: int,
+    start_index: int,
+    fixed_batch_size: bool,
+):
+    logger.info(f"Running inference on {test_file} using model {model_name_or_path}")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    data = load_data(test_file, num_samples, start_index)
+    model, tokenizer = load_model_tokenizer(model_name_or_path)
+    (
+        source,
+        source_ids,
+        categorical_slots_mapping,
+        slots,
+        sorting_index,
+    ) = get_sorted_data(data, tokenizer)
+    batches = dynamic_batches(
+        source,
+        tokenizer,
+        start_batch_size,
+        max_target_length,
+        safety_factor,
+        fixed_batch_size,
+    )
+    decoded_outputs = decode(
+        model, tokenizer, batches, device, max_target_length
+    )
+    reordered_outputs = reorder_outputs(decoded_outputs, sorting_index)
+    output_file = pathlib.Path(output_dir).joinpath("generated_predictions.txt")
+    with open(output_file, "w") as f:
+        f.write("\n".join(reordered_outputs))
+    logger.info(f"Predictions saved at {output_file}")
---
+++ scripts/sdt_scoring.py
@@ -0,0 +1,143 @@
+# Work by Ankit Bhattarai as part of Conversational AI Development project
+import json
+import logging
+from itertools import chain
+from pathlib import Path
+
+import click
+from datasets import load_dataset
+from omegaconf import OmegaConf
+from transformers import HfArgumentParser
+
+from robust_dst.cli import (
+    CustomSeq2SeqTrainingArguments,
+    DataTrainingArguments,
+    ModelArguments,
+)
+from robust_dst.evaluation import get_metrics
+from robust_dst.parser import SDTParser
+from robust_dst.scoring_utils import flatten_metrics_dict, setup_sgd_evaluation
+from robust_dst.utils import (
+    infer_data_version_from_path,
+)
+
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option(
+    "--initialisation_file_path",
+    type=click.Path(exists=True),
+    default="configs/train_sdt_baseline_cpu_debug.json",
+)
+@click.option("--data_type", type=click.Choice(["dev", "test"]), default="dev")
+@click.option("--predictions_file", type=click.Path(exists=True))
+@click.option("--refs_file", type=click.Path(exists=True))
+@click.option("--data_format", default="google")
+@click.option("--output_file", required=False, type=click.Path(exists=False))
+@click.option("--output_file_name", default="metrics.json", required=False)
+def main(
+    initialisation_file_path,
+    data_type,
+    predictions_file,
+    refs_file,
+    data_format,
+    output_file,
+    output_file_name,
+):
+    arg_parser = HfArgumentParser(
+        (ModelArguments, DataTrainingArguments, CustomSeq2SeqTrainingArguments)
+    )
+
+    model_args, data_args, training_args = arg_parser.parse_json_file(
+        json_file=initialisation_file_path
+    )
+    preprocessing_configs = {}  # type: dict[str, DictConfig]
+    data_files = {}  # type: dict[str, str]
+    extension = "json"
+
+    preprocessing_configs["validation"] = OmegaConf.load(
+        Path(data_args.validation_file).parent.joinpath("preprocessing_config.yaml")
+    )
+    preprocessing_configs["test"] = OmegaConf.load(
+        Path(data_args.test_file).parent.joinpath("preprocessing_config.yaml")
+    )
+
+    if data_args.validation_file is not None:
+        data_files["validation"] = data_args.validation_file
+        extension = data_args.validation_file.split(".")[-1]
+        data_version = infer_data_version_from_path(data_args.validation_file)
+        preprocessing_configs["validation"] = OmegaConf.load(
+            Path(data_args.validation_file).parent.joinpath("preprocessing_config.yaml")
+        )
+    if data_args.test_file is not None:
+        data_files["test"] = data_args.test_file
+        extension = data_args.test_file.split(".")[-1]
+        data_version = infer_data_version_from_path(data_args.test_file)
+        preprocessing_configs["test"] = OmegaConf.load(
+            Path(data_args.test_file).parent.joinpath("preprocessing_config.yaml")
+        )
+
+    raw_datasets = load_dataset(
+        extension, data_files=data_files, cache_dir=model_args.cache_dir, field="data"
+    )
+
+    raw_preprocessed_refs = {}  # type: dict[str, list[dict]]
+    for split in raw_datasets:
+        if split == "train":
+            continue
+        raw_preprocessed_refs[split] = raw_datasets.data[split].table.to_pylist()
+        if split == "test" and data_args.max_predict_samples is not None:
+            raw_preprocessed_refs[split] = raw_preprocessed_refs[split][
+                : data_args.max_predict_samples
+            ]
+        if split == "validation" and data_args.max_eval_samples is not None:
+            raw_preprocessed_refs[split] = raw_preprocessed_refs[split][
+                : data_args.max_eval_samples
+            ]
+
+    input_dtype = "validation" if data_type == "dev" else data_type
+    parser_inputs, sgd_evaluator_inputs = setup_sgd_evaluation(
+        data_args, preprocessing_configs, raw_preprocessed_refs, input_dtype
+    )
+
+    with open(refs_file, "r") as f:
+        gt = json.load(f)
+    gt = gt["data"]
+    with open(predictions_file, "r") as f:
+        hyps = f.readlines()
+    hyps = [item.replace(";", " ").strip() for item in hyps]
+
+    parser = SDTParser(
+        data_format=data_format,
+        template_dir=f"data/interim/blank_dialogue_templates/original/{data_type}",
+        schema_path=f"data/raw/original/{data_type}/schema.json",
+        files_to_parse=None,
+        dialogues_to_parse=None,
+    )
+    file_to_hyp_dials = parser.convert_to_sgd_format(
+        preprocessed_refs=gt,
+        predictions=hyps,
+    )
+    dataset_hyp = {
+        dial["dialogue_id"]: dial for dial in chain(*file_to_hyp_dials.values())
+    }
+
+    all_metrics_aggregate, _ = get_metrics(
+        dataset_ref=sgd_evaluator_inputs["dataset_ref"],
+        dataset_hyp=dataset_hyp,
+        service_schemas=sgd_evaluator_inputs["eval_services"],
+        in_domain_services=sgd_evaluator_inputs["in_domain_services"],
+    )
+    all_metrics_aggregate = flatten_metrics_dict(all_metrics_aggregate)
+    if output_file is None:
+        output_file = Path(predictions_file).parent.joinpath(output_file_name)
+    with open(output_file, "w") as f:
+        json.dump(all_metrics_aggregate, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
